dd4ca4a04dd5c8d10df3832942c673515a5a6856
chaeun99 <hyc9328@naver.com>
2025-12-24 22:15:08 +0900

    feat: ProductConverter 구현 (#3)

---
+++ src/main/java/com/shoppingmall/ecommerceapi/domain/product/converter/ProductConverter.java
@@ -0,0 +1,41 @@
+package com.shoppingmall.ecommerceapi.domain.product.converter;
+
+import com.shoppingmall.ecommerceapi.domain.product.dto.ProductCreateRequest;
+import com.shoppingmall.ecommerceapi.domain.product.dto.ProductResponse;
+import com.shoppingmall.ecommerceapi.domain.product.entity.Product;
+import com.shoppingmall.ecommerceapi.domain.product.entity.enums.ProductStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductConverter {
+
+  public Product toEntity(ProductCreateRequest request, String finalImgSrc, ProductStatus status) {
+    return Product.builder()
+        .name(request.getName())
+        .price(request.getPrice())
+        .description(request.getDescription())
+        .category(request.getCategory())
+        .stock(request.getStock() != null ? request.getStock() : 0)
+        .status(status)
+        .imgSrc(finalImgSrc)
+        .build();
+  }
+
+  public ProductResponse toResponse(Product product) {
+    if (product == null) {
+      return null;
+    }
+
+    return ProductResponse.builder()
+        .id(product.getId())
+        .name(product.getName())
+        .price(product.getPrice())
+        .category(product.getCategory().name())
+        .stock(product.getStock())
+        .status(product.getStatus().name())
+        .description(product.getDescription())
+        .imgSrc(product.getImgSrc())
+        .createdAt(product.getCreatedAt())
+        .build();
+  }
+}

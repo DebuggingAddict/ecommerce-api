9c0d56fb3caddd9623be024fdcac3938c6a5df6b
chaeun99 <hyc9328@naver.com>
2025-12-25 21:08:53 +0900

    feat: 상품 전체, 단건 조회 구현 (#4)

--- src/main/java/com/shoppingmall/ecommerceapi/domain/product/service/ProductService.java
+++ src/main/java/com/shoppingmall/ecommerceapi/domain/product/service/ProductService.java
@@ -1,14 +1,19 @@
 package com.shoppingmall.ecommerceapi.domain.product.service;
 
 import com.shoppingmall.ecommerceapi.common.exception.BusinessException;
+import com.shoppingmall.ecommerceapi.common.response.PageResponse;
 import com.shoppingmall.ecommerceapi.domain.product.converter.ProductConverter;
 import com.shoppingmall.ecommerceapi.domain.product.dto.ProductCreateRequest;
 import com.shoppingmall.ecommerceapi.domain.product.dto.ProductResponse;
 import com.shoppingmall.ecommerceapi.domain.product.entity.Product;
+import com.shoppingmall.ecommerceapi.domain.product.entity.enums.ProductCategory;
 import com.shoppingmall.ecommerceapi.domain.product.entity.enums.ProductStatus;
 import com.shoppingmall.ecommerceapi.domain.product.exception.ProductErrorCode;
 import com.shoppingmall.ecommerceapi.domain.product.repository.ProductRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +24,7 @@ public class ProductService {
   private final ProductRepository productRepository;
   private final ProductConverter productConverter;
 
+  // 상품 등록
   @Transactional
   public ProductResponse register(ProductCreateRequest request) {
     String finalImgSrc = (request.getImgSrc() == null) ? "none.png" : request.getImgSrc();
@@ -35,10 +41,27 @@ public class ProductService {
     return productConverter.toResponse(savedProduct);
   }
 
+  // 상품 단건 조회
   @Transactional(readOnly = true)
-  public Product findProductEntityById(Long id) {
+  public ProductResponse getProduct(Long id) {
     return productRepository.findById(id)
+        .filter(product -> product.getDeletedAt() == null)
+        .map(productConverter::toResponse)
         .orElseThrow(() -> new BusinessException(ProductErrorCode.PRODUCT_NOT_FOUND));
+  }
+
+  // 상품 전체 조회
+  @Transactional(readOnly = true)
+  public PageResponse<ProductResponse> getProducts(ProductCategory category, Pageable pageable) {
+    Page<Product> productPage = (category != null)
+        ? productRepository.findAllByCategoryAndDeleteAtIsNull(category, pageable)
+        : productRepository.findAllByDeleteAtIsNull(pageable);
+
+    List<ProductResponse> content = productPage.getContent().stream()
+        .map(productConverter::toResponse)
+        .toList();
+
+    return PageResponse.of(productPage, content, pageable.getSort().toString());
   }
 
   private boolean isValidImageExtension(String fileName) {
@@ -49,4 +72,9 @@ public class ProductService {
     return lowercase.endsWith(".jpg") || lowercase.endsWith(".png") || lowercase.endsWith(".jpeg");
   }
 
+  @Transactional(readOnly = true)
+  public Product findProductEntityById(Long id) {
+    return productRepository.findById(id)
+        .orElseThrow(() -> new BusinessException(ProductErrorCode.PRODUCT_NOT_FOUND));
+  }
 }

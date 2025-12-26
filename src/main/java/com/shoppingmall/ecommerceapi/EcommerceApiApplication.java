da6ed0d71727fd67d9b699e039002c14bafb60b6
BPdog <dkstl3344@gmail.com>
2025-12-26 21:11:15 +0900

    fix: 컨트롤러 테스트(@WebMvcTest)에서 Auditing 비활성화 - @EnableJpaAuditing을 메인 클래스에서 분리하고, test 프로파일에서는 Auditing 설정을 제외함

--- src/main/java/com/shoppingmall/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/shoppingmall/ecommerceapi/EcommerceApiApplication.java
@@ -3,14 +3,11 @@ package com.shoppingmall.ecommerceapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class EcommerceApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EcommerceApiApplication.class, args);
   }
-
 }
---
+++ src/main/java/com/shoppingmall/ecommerceapi/config/jpa/JpaAuditingConfig.java
@@ -0,0 +1,12 @@
+package com.shoppingmall.ecommerceapi.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Profile("!test")
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}

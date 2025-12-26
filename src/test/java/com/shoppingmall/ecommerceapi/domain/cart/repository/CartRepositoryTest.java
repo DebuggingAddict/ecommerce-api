bd51510198db70525e40de31c2850057d034be04
BPdog <dkstl3344@gmail.com>
2025-12-27 00:05:38 +0900

    fix: CartRepositoryTest에서 JpaAuditingConfig 로딩 추가

--- src/test/java/com/shoppingmall/ecommerceapi/domain/cart/repository/CartRepositoryTest.java
+++ src/test/java/com/shoppingmall/ecommerceapi/domain/cart/repository/CartRepositoryTest.java
@@ -2,6 +2,7 @@ package com.shoppingmall.ecommerceapi.domain.cart.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.shoppingmall.ecommerceapi.config.jpa.JpaAuditingConfig;
 import com.shoppingmall.ecommerceapi.domain.cart.entity.Cart;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -9,9 +10,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(JpaAuditingConfig.class)
 class CartRepositoryTest {
 
   private final CartRepository cartRepository;

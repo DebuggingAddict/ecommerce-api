6744fe3e67b540f5587f2c4e63291709095d6e67
chaeun99 <hyc9328@naver.com>
2025-12-27 15:58:42 +0900

    feat: ResponseStatus 추가 (#3)

--- src/main/java/com/shoppingmall/ecommerceapi/domain/product/controller/AdminProductController.java
+++ src/main/java/com/shoppingmall/ecommerceapi/domain/product/controller/AdminProductController.java
@@ -7,12 +7,14 @@ import com.shoppingmall.ecommerceapi.domain.product.dto.ProductUpdateRequest;
 import com.shoppingmall.ecommerceapi.domain.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -24,6 +26,7 @@ public class AdminProductController {
 
   // 상품 등록
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Api<ProductResponse> create(
       @Valid @RequestBody ProductCreateRequest request
   ) {

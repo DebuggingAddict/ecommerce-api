fcb1a12cc06a19a3a060025fa79aa4bfbf5959ae
BPdog <dkstl3344@gmail.com>
2025-12-28 11:32:55 +0900

    fix: cart API 엔드포인트 /api/cart로 수정

--- src/main/java/com/shoppingmall/ecommerceapi/domain/cart/controller/CartController.java
+++ src/main/java/com/shoppingmall/ecommerceapi/domain/cart/controller/CartController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/cart")
+@RequestMapping("/api/cart")
 public class CartController {
 
   private final CartService cartService;

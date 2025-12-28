f7e4dac928f558428c89eec8254cbc736c727ae6
chaeun99 <hyc9328@naver.com>
2025-12-28 16:19:21 +0900

    test: 테스트 전용 데이터베이스 환경 구축 (#4)

--- src/test/java/com/shoppingmall/ecommerceapi/domain/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/shoppingmall/ecommerceapi/domain/product/repository/ProductRepositoryTest.java
@@ -14,14 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-@Import(ProductRepositoryTest.TestConfig.class)
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductRepositoryTest {
 

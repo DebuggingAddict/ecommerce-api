287c2a286e5f495c06fca64eed20eb07f2b0ab07
chaeun99 <hyc9328@naver.com>
2026-01-02 10:22:54 +0900

    feat: S3 인터페이스 추가 (#68)

--- src/main/java/com/shoppingmall/ecommerceapi/common/infra/S3Service.java
+++ src/main/java/com/shoppingmall/ecommerceapi/common/infra/S3Service.java
@@ -1,42 +1,12 @@
 package com.shoppingmall.ecommerceapi.common.infra;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.shoppingmall.ecommerceapi.common.exception.BusinessException;
-import com.shoppingmall.ecommerceapi.domain.product.exception.ProductErrorCode;
-import java.io.IOException;
-import java.util.UUID;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Service
-@RequiredArgsConstructor
-public class S3Service {
+public interface S3Service {
 
-  private final AmazonS3 amazonS3;
+  // 파일 업로드
+  String uploadFile(MultipartFile file);
 
-  @Value("${cloud.aws.s3.bucket}")
-  private String bucket;
-
-  public String uploadFile(MultipartFile file) {
-    if (file == null || file.isEmpty()) {
-      return "none.png";
-    }
-
-    String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-    try {
-      ObjectMetadata metadata = new ObjectMetadata();
-      metadata.setContentLength(file.getSize());
-      metadata.setContentType(file.getContentType());
-
-      amazonS3.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata));
-
-      return amazonS3.getUrl(bucket, fileName).toString();
-    } catch (IOException e) {
-      throw new BusinessException(ProductErrorCode.PRODUCT_INVALID_IMAGE);
-    }
-  }
+  // 파일 삭제
+  void deleteFile(String fileUrl);
 }

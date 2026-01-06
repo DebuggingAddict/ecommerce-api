5654485ba343c322c8694a22674fec2c23f7f001
BPdog <dkstl3344@gmail.com>
2026-01-06 10:24:23 +0900

    fix: 토큰 만료시간 설정을 static 필드에서 상수(3600초)로 변경

--- src/main/java/com/shoppingmall/ecommerceapi/domain/auth/dto/TokenResponse.java
+++ src/main/java/com/shoppingmall/ecommerceapi/domain/auth/dto/TokenResponse.java
@@ -2,7 +2,6 @@ package com.shoppingmall.ecommerceapi.domain.auth.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 @Builder
@@ -12,16 +11,12 @@ public class TokenResponse {
     private String tokenType;
     private Long expiresIn;
 
-    @Value("${token.access-token.plus-hour}")
-    static Long EXPIRATION_TIME; // 1시간 (밀리초 단위)
-
-
     public static TokenResponse of(String accessToken, String refreshToken) {
         return TokenResponse.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .tokenType("Bearer")
-                .expiresIn(EXPIRATION_TIME * 3600L) // 1시간 (초 단위)
+                .expiresIn(3600L) // 1시간 (초 단위)
                 .build();
     }
 }
